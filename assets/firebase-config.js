ecdf2a4c74abdd877d6a966fa5c3510c7a2bc3c5
dewantara-studio <sonyputradewantara@gmail.com>
2026-09-02 10:13:11 +0700

    Update firebase-config.js

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -25,5 +25,5 @@ auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
 /* Daftar email yang boleh membuka Dashboard Admin.
    Tambahkan email staf lain dengan koma di antaranya. */
 const ADMIN_EMAILS = [
-  "sdi.tompokersanlumajang@gmail.com"
+  "izalainufar@sditompokersan.sch.id"
 ];
